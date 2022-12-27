4c394279d6ee5c428bed50e9e7a53a338e2e5cb0
Alfin89 <104522708+Alfin89@users.noreply.github.com>
2022-12-27 16:06:41 +0700

    Add files via upload
    menambhakan 2 app dan merubah tampilan

--- serviceapp/apps.py
+++ serviceapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PerpusappConfig(AppConfig):
+class ServiceappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'perpusapp'
+    name = 'serviceapp'
